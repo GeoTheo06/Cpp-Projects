40675088da10f1fc5b21136d6b70564a2f9fe3fb
GeoTheo06 <theodorgeor@gmail.com>
2022-12-22 00:37:19 +0200

    trying to find out what the f*ck is going on jk. pretty  close

--- diagwnismos 22-23 a fash/Diagwnismos_22-23_a_fash.cpp
+++ diagwnismos 22-23 a fash/Diagwnismos_22-23_a_fash.cpp
@@ -1,66 +1,49 @@
-#include <iostream> 
-#include <fstream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	ifstream in("coupon.in");
-	ofstream out("coupon.out");
-	int n = 0, b = 0, sum = 0;
-	float a = 0, sum_x1 = 0;
-	float* x1 = new float[n];
-	/*
-	n = poses oikonomikes omades (6)
-	a = poso to opoio katevazei to poso pou tha parei h kathe oikonomikh omada (0.5)
-	b = synoliko poso pou tha paroun oles oi omades mazi (1000000)
-	c[n] = poses oikogeneies exei h kathe oikonomikh omada
-  */
-	in >> n >> a >> b;
-	int* c = new int[n];
-	float* x = new float[n];
-	for (int i = 0; i < n; i++) {
-		in >> c[i];
+int main() {
+    ifstream in("coupon.in");
+    ofstream out("coupon.out");
 
-	  //initializing x
-		x[i] = 0;
-	}
+    int n = 0, b = 0, x1 = 0, sum = 0;
+    float a = 0;
+    int *c = new int[n];
+    float *x = new float[n];
+    float *x_result = new float[n];
 
+    in >> n >> a >> b;
+    for (int i = 0; i < n; i++) {
+        in >> c[i];
+    }
 
+    x1 = 100;  // temporary (set it to something like b / c[0])
+    int tempSize = x1;
+    x1 += 1;
+    x[0] = x1;
+    for (int i = 0; i <= tempSize; i++) {
+        x1 -= 1;
 
-	for (int i = 0; i < n; i++) {
-		if (i == 0) {
-			if (c[i] >= 10) {
-				x1[i] = c[i];
-			}
-		} else {
-			if (c[i] * pow(a, i) >= 10)
-				x1[i] = c[i] * pow(a, i);
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		sum_x1 += x1[i];
-	}
+        for (int k = 0; k < n; k++) {
+            x[k] = k == 0
+                       ? x1
+                       : x[k - 1] * 0.8;  // is this ok? pros thn k. efraimidou
+            if (x[k] >= 10) {
+                x_result[k] = x[k] * c[k];
+                sum = sum + x_result[k];
+            } else {
+                x[k] = 0;
+                x_result[k] = 0;
+            }
+        }
 
-	sum_x1 = b / sum_x1;
-	x[0] = sum_x1;
-	for (int i = 0; i < n; i++) {
-		if (sum_x1 * pow(a, i) >= 10) {
-			x[i] = sum_x1 * pow(a, i);
-		} else {
-			x[i] = 0;
-		}
-		sum += x[i] * c[i];
-	}
-	out << sum << endl;
+        if (sum <= b) break;
+    }
 
-	//printing output
-	for (int i = 0; i < n; i++) {
-		out << x[i];
+    // printing output
+    out << sum;
+    for (int i = 0; i < n; i++) {
+        out << endl << x[i];
+    }
 
-		if (i != n - 1) {
-			out << endl;
-		}
-	}
-	delete[] c, x;
+    delete[] c, x, x_result;
 }
