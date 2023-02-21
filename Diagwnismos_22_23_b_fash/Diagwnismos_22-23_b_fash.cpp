91db47d5f75a3c09a0ab4b8d6b4e489585d15904
GeoTheo06 <theodorgeor@gmail.com>
2023-02-21 22:22:29 +0200

    thought of an approach. seems like i am going to need loops inside of loops which is unefficient af. i believe something like recursion and backtracking will do the trick

--- Diagwnismos_22_23_b_fash/Diagwnismos_22-23_b_fash.cpp
+++ Diagwnismos_22_23_b_fash/Diagwnismos_22-23_b_fash.cpp
@@ -42,18 +42,49 @@ int main()
 
 	// tha metraei oles tis prosfores pou to mhkos tous periexei to arxiko xiliometro tou erwthmatos
 	vector<int> prosforesPrinToArxikoXiliometroErwthmatos;
+	vector<int> olesOiPithanesProsforesMetaThnPrwth;
+	vector<vector<int>> chain;
 
 	// logic
 	for (int i = 0; i < erwthmata; i++)
 	{
-		// vriskw oles tis prosfores twn opoiwn to mhkos periexei to arxiko xiliometro tou erwthmatos kai apothikevw ton arithmo ths prosforas sto prosforesPrinToArxikoXiliometroErwthmatos
-		for (int k = 0; k < prosfores; k++)
-		{
-			if (arxikoXiliometroProsforas[k] <= arxikoXiliometroErwthmatos[i] && telikoXiliometroProsforas[k] >= arxikoXiliometroErwthmatos[i])
-				prosforesPrinToArxikoXiliometroErwthmatos.push_back(k);
-		}
 
-		// continue from here (here goes the rest of the logic)
+
+//*******// KANTO ME RECURSION (backtrack) gia na vreis olous tous pithanous syndiasmous. Meta, des poios einai o pio fthinos //
+
+
+
+
+
+		// // vriskw oles tis prosfores twn opoiwn to mhkos periexei to arxiko xiliometro tou erwthmatos kai apothikevw ton arithmo ths prosforas sto prosforesPrinToArxikoXiliometroErwthmatos
+		// for (int k = 0; k < prosfores; k++)
+		// {
+		// 	if (arxikoXiliometroProsforas[k] <= arxikoXiliometroErwthmatos[i] && telikoXiliometroProsforas[k] >= arxikoXiliometroErwthmatos[i])
+		// 	{
+		// 		prosforesPrinToArxikoXiliometroErwthmatos.push_back(k);
+		// 	}
+		// 	else if (arxikoXiliometroProsforas[k] > telikoXiliometroErwthmatos[i])
+		// 	{
+		// 		olesOiPithanesProsforesMetaThnPrwth.push_back(k);
+		// 	}
+		// }
+		// chain.push_back(prosforesPrinToArxikoXiliometroErwthmatos);
+
+		// // prosfores prin to arxiko xiliometro erwthmatos counter
+		// // int ptaxeCounter = 0;
+		// // edw tha vrw olous tous syndyasmous pou mporoun na ginoun ksekinontas apo tis prosfores pou periexontai sto prosforesPrinToArxikoXiliometroErwthmatos
+		// for (int k = 0; k < prosforesPrinToArxikoXiliometroErwthmatos.size(); k++)
+		// {
+		// 	for (int l = 0; l < olesOiPithanesProsforesMetaThnPrwth.size(); l++)
+		// 	{
+		// 		if (arxikoXiliometroProsforas[olesOiPithanesProsforesMetaThnPrwth[l]] <= telikoXiliometroProsforas[prosforesPrinToArxikoXiliometroErwthmatos[k]] && telikoXiliometroProsforas[olesOiPithanesProsforesMetaThnPrwth[l]] > telikoXiliometroProsforas[prosforesPrinToArxikoXiliometroErwthmatos[k]])
+		// 		{
+		// 			chain.push_back(prosforesPrinToArxikoXiliometroErwthmatos)
+		// 		}
+		// 	}
+		// }
+		// prosforesPrinToArxikoXiliometroErwthmatos.clear();
+		// olesOiPithanesProsforesMetaThnPrwth.clear();
 	}
 
 	delete[] arxikoXiliometroProsforas, mhkosProsforas, kostosProsforas, telikoXiliometroProsforas, arxikoXiliometroErwthmatos, mhkosErwthmatos;
