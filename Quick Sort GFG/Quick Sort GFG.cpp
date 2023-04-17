579121fcc0e866fea978d661c82dd720fc5a705a
GeoTheo06 <theodorgeor@gmail.com>
2023-04-17 11:02:25 +0300

    completed quicksort problem (it was easy after the other 2)

--- Quick Sort GFG/Quick Sort GFG.cpp
+++ Quick Sort GFG/Quick Sort GFG.cpp
@@ -38,18 +38,66 @@ Constraints:
 #include <iostream>
 using namespace std;
 
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d", arr[i]);
+	printf("\n");
+}
+
 class Solution
 {
 public:
 //Function to sort an array using quick sort algorithm.
 	void quickSort(int arr[], int low, int high)
 	{
-		// Your code here
+		if (low < high)
+		{
+			int pivot = partition(arr, low, high);
+
+			quickSort(arr, low, pivot - 1);
+			quickSort(arr, pivot + 1, high);
+		}
 	}
 
-public:
+	int swap(int arr[], int a, int b)
+	{
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+
 	int partition(int arr[], int low, int high)
 	{
-	   // Your code here
+		int b = low - 1;
+		for (int i = low; i < high; i++)
+		{
+			if (arr[i] <= arr[high])
+			{
+				b++;
+				swap(arr, b, i);
+			}
+		}
+		swap(arr, b + 1, high);
+		return b + 1;
 	}
 };
+
+int main()
+{
+	int arr[1000], n, T, i;
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%d", &n);
+		for (i = 0; i < n; i++)
+		{
+			scanf("%d", &arr[i]);
+			Solution ob;
+			ob.quickSort(arr, 0, n - 1);
+			printArray(arr, n);
+		}
+		return 0;
+	}
+}
